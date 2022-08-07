940e70574e832eeb2bda848dc7c7a46305a8cdd6
Mokate Om <110752873+MokateOm16@users.noreply.github.com>
2022-08-07 10:32:36 +0530

    Add files via upload

---
+++ find_string_pelimdrome_or_not.py
@@ -0,0 +1,12 @@
+
+# Name    :- Om Mokate
+# Program :- Dead Calculator
+#Date     :- 29 July 2022
+
+str = input("enter a string = ")
+if str == str[::-1]:
+    print("yes , it is pelimdrom ")
+else:
+    print("No , it is not pelimdrom ")
+    
+
